555de79d9f6385903ce01c1f95dbb7e346f3248e
s-prechtl <stefan@tague.at>
2023-02-18 22:07:07 +0100

    reveal, toggle flag, count surronding unfinished

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ impl Cell {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Field (Vec<Vec<Cell>>);
 
 impl Field {
@@ -26,10 +26,38 @@ impl Field {
 
         Self(field)
     }
+
+    /// Reveals the given points Field
+    /// @return If cells is a bomb
+    pub fn reveal(&mut self, pos: Point2) -> bool{
+        let mut cell = self.0[pos.y as usize][pos.x as usize];
+        cell.is_revealed = true;
+
+        cell.is_bomb
+    }
+
+
+
+    fn count_surrounding_bombs(&self, pos: Point2) -> u8 {
+        let mut bombs = 0;
+        if pos.x as u32 > 0 {
+            bombs += self.0[pos.y as usize][pos.x as usize-1].is_bomb as usize;
+        }
+
+        bombs as u8
+    }
+
+    fn set_bomb_counts(&mut self) {
+        let mut field = self.clone();
+        for (y, row) in field.0.iter_mut().enumerate() {
+            for (x,  cell) in row.iter_mut().enumerate() {
+                cell.bomb_count = self.count_surrounding_bombs(Point2::new(x as f32, y as f32));
+            }
+        }
+    }
 }
 
 fn main() {
-    dbg!(Field::empty(10, 10));
     nannou::sketch(view).run()
 }
 
