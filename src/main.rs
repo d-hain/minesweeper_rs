d80eb30809be2676dd71d4d0fdecb2173e4bbffd
s-prechtl <stefan@tague.at>
2023-02-18 20:28:02 +0100

    Created structs and intial field stuff dies das

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,35 @@
 use nannou::prelude::*;
 
+#[derive(Default, Clone, Copy, Debug)]
+struct Cell {
+    has_flag: bool,
+    is_bomb: bool,
+    bomb_count: u8,
+    is_revealed: bool,
+}
+
+impl Cell {
+    pub fn new(is_bomb: bool) -> Self {
+        Self {
+            is_bomb,
+            ..Default::default()
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Field (Vec<Vec<Cell>>);
+
+impl Field {
+    pub fn empty(rows: u8, cols: u8) -> Self {
+        let field = vec![vec![Cell::new(false); cols as usize];rows as usize];
+
+        Self(field)
+    }
+}
+
 fn main() {
+    dbg!(Field::empty(10, 10));
     nannou::sketch(view).run()
 }
 
