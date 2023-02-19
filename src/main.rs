eeaab01e4bff81eb614560c484a0a72954227a3b
s-prechtl <stefan@tague.at>
2023-02-19 14:26:38 +0100

    Formatted

--- src/main.rs
+++ src/main.rs
@@ -233,9 +233,17 @@ impl Field {
                 }
 
                 if cell.has_flag {
-                    draw.tri()
-                        .color(STEELBLUE)
-                        .points(Point2::new(cell_x_pos-model.cell_width/2.0, cell_y_pos-model.cell_height/2.0), Point2::new(cell_x_pos-model.cell_width/2.0, cell_y_pos+model.cell_height/2.0), Point2::new(cell_x_pos+model.cell_width/2.0, cell_y_pos));
+                    draw.tri().color(STEELBLUE).points(
+                        Point2::new(
+                            cell_x_pos - model.cell_width / 2.0,
+                            cell_y_pos - model.cell_height / 2.0,
+                        ),
+                        Point2::new(
+                            cell_x_pos - model.cell_width / 2.0,
+                            cell_y_pos + model.cell_height / 2.0,
+                        ),
+                        Point2::new(cell_x_pos + model.cell_width / 2.0, cell_y_pos),
+                    );
                 }
             }
         }
